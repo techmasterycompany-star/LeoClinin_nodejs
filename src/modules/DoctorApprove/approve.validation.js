a0b423fecb687bda7621a4df4f6f7ff6c2e2d4ce
AhmedHassan-Ahmed <ahmedhassan8754321@gmail.com>
2026-08-04 22:22:17 +0300

    add doctor-Approve-Api

--- src/modules/DoctorApprove/approve.validation.js
+++ src/modules/DoctorApprove/approve.validation.js
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const acceptAndRejectSchema = z.object({
   params: z.object({
-    id: z.string().min(1, "doctor ID is required"),
+    doctorId: z.string().min(1, "doctor ID is required"),
   }),
 });
