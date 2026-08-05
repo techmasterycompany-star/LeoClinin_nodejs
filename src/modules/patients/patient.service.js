caf7a5d9666d3d36e62c56cb00ade05b5cfbbc90
ArkanKira <arkanbasha140@gmail.com>
2026-08-05 13:17:07 +0300

    Refactor updatePatientPrivateInfo to use AppError
    Replaced error handling with AppError for better error management.

--- src/modules/patients/patient.service.js
+++ src/modules/patients/patient.service.js
@@ -1,11 +1,12 @@
 import User from '../../models/User.model.js';
+import AppError from '../../error/AppError.js';
 
 export const updatePatientPrivateInfo = async (userId, updateData) => {
   const user = await User.findById(userId);
-  if (!user) throw new Error('User not found');
+  if (!user) throw new AppError('User not found', 404);
   
   if (user.role !== 'patient') {
-    throw new Error('Forbidden');
+    throw new AppError('Only patients can update this info', 403);
   }
 
   if (updateData.address) user.patientProfile.address = updateData.address;
