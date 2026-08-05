ddcf3dce4c9617d7d44584a25c33d352000e7497
ArkanKira <arkanbasha140@gmail.com>
2026-08-05 13:15:49 +0300

    Add validation middleware to user update route

--- src/modules/users/user.route.js
+++ src/modules/users/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { authMiddleware } from '../../middlewares/auth.middleware.js';
+import { validate } from '../../middlewares/validation.middleware.js';
+import { updateUserSchema } from './user.validation.js';
 import { updateMe } from './user.controller.js';
 
 const router = Router();
 
-router.patch('/me', authMiddleware, updateMe);
+router.patch('/me', authMiddleware, validate(updateUserSchema), updateMe);
 
 export default router;
