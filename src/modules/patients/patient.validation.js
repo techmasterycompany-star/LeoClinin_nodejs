4151510373e939394cd2c93bbdca85d2ad7c974b
ArkanKira <arkanbasha140@gmail.com>
2026-08-05 13:16:10 +0300

    Refactor updatePatientSchema to include body object

--- src/modules/patients/patient.validation.js
+++ src/modules/patients/patient.validation.js
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
 export const updatePatientSchema = z.object({
-  address: z.string().optional(),
-  date_of_birth: z.string().datetime().optional(),
+  body: z.object({
+    address: z.string().optional(),
+    date_of_birth: z.string().datetime().optional(),
+  }),
 });
