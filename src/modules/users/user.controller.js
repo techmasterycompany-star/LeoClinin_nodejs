3f855196330bedcdbb97ed2fbeca673acbaf36ca
ArkanKira <arkanbasha140@gmail.com>
2026-08-05 14:23:20 +0300

    Update user.controller.js
    Added getAllUsers function to retrieve user data.

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -1,4 +1,4 @@
-import { updateUserBasicInfo } from './user.service.js';
+import { updateUserBasicInfo, getUsers } from './user.service.js';
 
 export const updateMe = async (req, res, next) => {
   const updatedUser = await updateUserBasicInfo(req.user._id, req.body);
@@ -6,5 +6,13 @@ export const updateMe = async (req, res, next) => {
     success: true,
     message: 'Profile updated successfully',
     data: updatedUser,
+  });
+};
+
+export const getAllUsers = async (req, res, next) => {
+  const result = await getUsers(req.query);
+  res.status(200).json({
+    success: true,
+    data: result,
   });
 };
