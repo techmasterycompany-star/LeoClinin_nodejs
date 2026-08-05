1c33a5fbc114df4ffc94f914c522dab60688207a
ArkanKira <arkanbasha140@gmail.com>
2026-08-05 14:24:42 +0300

    Update user.route.js
    Added a new route to get all users with authorization and validation.

--- src/modules/users/user.route.js
+++ src/modules/users/user.route.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
-import { authMiddleware } from '../../middlewares/auth.middleware.js';
+import { authMiddleware, authorize } from '../../middlewares/auth.middleware.js';
 import { validate } from '../../middlewares/validation.middleware.js';
-import { updateUserSchema } from './user.validation.js';
-import { updateMe } from './user.controller.js';
+import { updateUserSchema, getUsersSchema } from './user.validation.js';
+import { updateMe, getAllUsers } from './user.controller.js';
 
 const router = Router();
 
 router.patch('/me', authMiddleware, validate(updateUserSchema), updateMe);
+
+router.get(
+  '/',
+  authMiddleware,
+  authorize('admin'),
+  validate(getUsersSchema),
+  getAllUsers
+);
 
 export default router;
